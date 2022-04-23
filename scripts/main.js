a00840a5058c587bb7fe45d658670a03a0a38f22
Brandon Lisonbee <blisonbee213596@gmail.com>
2022-04-23 11:17:49 -0600

    new home page commit

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,9 @@ let currentdate = document.lastModified;
 document.querySelector('#lastdate').textContent = currentdate;
 
 console.log(currentdate);
+
+let currentyear = date.getFullYear();
+
+document.querySelector('#currentyear').textContent = currentyear;
+
+console.log(currentyear);
