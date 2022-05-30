4ef5e1dd91014a1ff9a44d4311a99a1ff9358c2a
Brandon Lisonbee <blisonbee213596@gmail.com>
2022-05-30 12:43:00 -0600

    wind chill commit

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,12 @@
+function do_wind_chill(){
+
+    let t = (document.querySelector("#temp span").textContent)*1
+    let s = (document.querySelector("#speed span").textContent)*1
+    let c = t;
+    if (t <= 50 && s >= 3){
+        c = Math.round((35.74 + (0.6215 * t))-(35.75 * Math.pow(s,0.16)) + (0.4275*t*Math.pow(s,0.16)));
+    }
+    document.querySelector("#chill span").textContent = c
+}
+
+do_wind_chill();
