b7b029496c363470f2d5eb3182172e4a73a0f34f
Brandon Lisonbee <blisonbee213596@gmail.com>
2022-06-05 02:16:34 -0600

    discover commit

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,5 +1,5 @@
 // select the elements to manipulate (output to)
-const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+const datefieldUK = document.querySelector("#date"); // for european/family history format with day first.
 
 // derive the current date using a date object
 const now = new Date();
@@ -34,3 +34,58 @@ const currentDay = date.getDay();
 if (currentDay == 1 || currentDay == 2) {
     banner.style.display = 'block';
 }
+
+
+// ------ Last Visited ------ //
+
+let visit_heading = document.querySelector('.visited');
+
+let now_date = new Date();
+
+let stored_date = localStorage.getItem('last_visited');
+let last_date = new Date(stored_date);
+
+if (!stored_date) {
+    visit_heading.textContent = 'Welcome to the Discover Page!';
+}
+else {
+    let diff_time = now_date.getTime() - last_date.getTime();
+
+    let diff_days = Math.round(diff_time/(1000 * 60 * 60 * 24));
+
+    visit_heading.textContent = diff_days + ' days since your last visit';
+}
+
+localStorage.setItem('last_visited', now_date);
+
+
+// ------ Lazy Loading ------ //
+
+const images = document.querySelectorAll('[data-src]');
+const options = {threshold: .5, rootMargin: "0px 0px 100px 0px"};
+
+function preloadImage(img) {
+    const source = img.getAttribute('data-src');
+    if(!source) {
+        return;
+    }
+    
+    img.src = source;
+}
+
+const io = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                io.unobserve(entry.target);
+            }
+        })
+    }, options
+);
+
+images.forEach(image => {
+    io.observe(image);
+});
