bc63dad759e9c5e7e8d325189f1ada23f58b48db
Brandon Lisonbee <blisonbee213596@gmail.com>
2022-07-19 11:46:09 -0600

    various fixes

--- temple/scripts/main.js
+++ temple/scripts/main.js
@@ -13,24 +13,6 @@ document.querySelector('#lastdate').textContent = currentdate;
 
 
 
-// slide show
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 7000); // Change image every 7 seconds
-}
-
-
 // days since last visit
 
 let visit_heading = document.querySelector('.visited');
