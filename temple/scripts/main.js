c4b1f6c808873eb6ea677cd0c93209169be6c355
Brandon Lisonbee <blisonbee213596@gmail.com>
2022-07-19 09:13:35 -0600

    last visited commit

--- temple/scripts/main.js
+++ temple/scripts/main.js
@@ -35,3 +35,27 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 7000); // Change image every 7 seconds
 }
+
+
+// days since last visit
+
+let visit_heading = document.querySelector('.visited');
+
+let now_date = new Date();
+
+let stored_date = localStorage.getItem('last_visited');
+let last_date = new Date(stored_date);
+
+if(visit_heading){
+if (!stored_date) {
+    visit_heading.textContent = 'Welcome!';
+}
+else {
+    let diff_time = now_date.getTime() - last_date.getTime();
+
+    let diff_days = Math.round(diff_time/(1000 * 60 * 60 * 24));
+
+    visit_heading.textContent = 'Welcome! It has been ' + diff_days + ' days since your last visit';
+    }
+}
+localStorage.setItem('last_visited', now_date);
